8e06fba5fe9204a559f9b75b55b2f5cad62a10f0
NiyazUrazaev <niyazurazaev@gmail.com>
2020-05-07 16:57:51 +0300

    Create apps for mindfullness and gamification

--- SummerPractice/settings.py
+++ SummerPractice/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'user',
     'practice',
+    'gamification',
+    'mindfulness',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
