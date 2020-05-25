5ca2e0e6b9034c6b013980034b8570827f427b78
NiyazUrazaev <niyazurazaev@gmail.com>
2020-05-25 22:22:20 +0300

    Revert "Test wsgi"
    This reverts commit 24132419

--- SummerPractice/wsgi.py
+++ SummerPractice/wsgi.py
@@ -14,6 +14,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SummerPractice.settings')
 git.cmd.Git('.').pull()
-print('start wsgi')
 
 application = get_wsgi_application()
